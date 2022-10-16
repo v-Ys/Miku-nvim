afa6351ae675d6d7a9d38610621926a6337544ba
Vv-Mys <vincent.vv.mys@gmail.com>
2022-10-16 16:05:18 +0800

    remove file

--- lua/Miku-vim/conf/toggleterm.lua
+++ lua/Miku-vim/conf/toggleterm.lua
@@ -5,7 +5,7 @@ end
 
 toggleterm.setup({
         size = 20,
-        open_mapping = [[<c-m>]],
+        open_mapping = [[<A-m>]],
         hide_numbers = true,
         shade_filetypes = {},
         shade_terminals = true,
