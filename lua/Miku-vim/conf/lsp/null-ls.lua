4d97c4950f282d561053913c770405f712057a21
Vv-Mys <vincent.vv.mys@gmail.com>
2022-10-16 16:10:03 +0800

    spell

--- lua/Miku-vim/conf/lsp/null-ls.lua
+++ lua/Miku-vim/conf/lsp/null-ls.lua
@@ -1,17 +1,16 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-    return
+        return
 end
 
 
 local formatting = null_ls.builtins.formatting
--- local completion = null_ls.builtins.completion
+local completion = null_ls.builtins.completion
 
 null_ls.setup({
-    debug = false,
-    sources = {
-        formatting.autopep8, -- for python
-        -- formatting.clang_format, -- for cpp
-        -- completion.spell,
-    },
+        debug = false,
+        sources = {
+                formatting.autopep8, -- for python
+                completion.spell,
+        },
 })
