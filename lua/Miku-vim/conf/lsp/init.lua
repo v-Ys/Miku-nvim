4e1d89e37a0a63d90721abbcbc47040d19397b47
Vv-ys <vincent.vv.mys@gmail.com>
2023-02-24 21:58:55 +0800

    delete old files

--- lua/Miku-vim/conf/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-        return 'lsp.config'
-end
-require "Miku-vim.conf.lsp.config"
-require "Miku-vim.conf.lsp.null-ls"
--- require "Miku-vim.conf.lsp.lsp_signature"
-require("Miku-vim.conf.lsp.handlers").setup()
